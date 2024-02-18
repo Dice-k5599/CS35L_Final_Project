72adb9deb33ee03d10209418ff93b318c6dcdbda
RyanPosti <ryanposti01@gmail.com>
2024-02-18 15:47:38 -0800

    Added radio buttons and checklists to be used for the entering of user data when selecting what classes they have completed

--- react_dars_website/src/App.js
+++ react_dars_website/src/App.js
@@ -1,37 +1,67 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css'
 
+function GE({complete}){
+    if(complete==="GE False"){
+        return (
+            <div>
+                <div> <b>Please select completed GE requirements:</b> </div>
+                <div style={{marginLeft: "20px"}}>
+                <input type="checkbox" id="geReq1" name="geReq1" value="geReq1"/>
+                GE Requirement1
+                </div>
+            </div>
+        );
+    }
+    return null;
+}
+
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {  // state is something that could change
-            robots: robots,
-            searchfield: ''
-        }
+    constructor(props){
+        super(props);
+        this.state={
+            selectedOption: "GE True"
+        };
     }
-
-    componentDidMount() {
-        
-    }
-
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value }); //need to do this instead of this.state.searchfield = value
+    handleOptionChange= changeEvent =>{
+        this.setState({selectedOption: changeEvent.target.value})
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-
         return (    
-            <div className='tc'>
-                <h1><strong>Lakers Players Index</strong></h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
+            <form>
+                <div>
+                    <b>Have you Completed your General Education?</b>
+                </div>
+                <div className='GE Completed' style={{marginLeft: "20px"}}>
+                    <label>
+                        <input type="radio" value="GE True" checked={this.state.selectedOption==="GE True"} 
+                        onChange={this.handleOptionChange} />
+                        GE Completed
+                    </label>
+                </div>
+                <div className='GE Not' style={{marginLeft: "20px"}}>
+                    <label>
+                        <input type="radio" value="GE False" checked={this.state.selectedOption==="GE False"}
+                        onChange={this.handleOptionChange} />
+                        GE Not Completed
+                    </label>  
+                </div>
+                <div>
+                    <GE
+                    complete={this.state.selectedOption}
+                    />
+                </div>
+                <div>
+                    <b>Major Requirements</b>
+                </div>
+                <div style={{marginLeft: "20px"}}>
+                    <label>
+                    <input type="checkbox" id="mReq1" name="mReq1" value="mReq1"/>
+                    Major Requirement1
+                    </label>
+                </div>
+            </form>
         );
     }
 }
