c6c2053d65fbbabc8b785c060b6e337a57192818
Dice-k5599 <108194726+Dice-k5599@users.noreply.github.com>
2024-02-29 16:46:45 -0800

    Removed code that loads YT's signin page in index.js

--- react_dars_website/src/index.js
+++ react_dars_website/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
@@ -10,8 +10,7 @@ import LoginForm from './loginForm';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginForm/>
-    {/* <App/> */}
+    <App/>
   </React.StrictMode>
 );
 
