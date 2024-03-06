cffd7afcbb497b99d859712d8dde6f7a65a92e02
RyanPosti <ryanposti01@gmail.com>
2024-03-06 14:17:43 -0800

    Saving for future use

--- react_dars_website/src/App.js
+++ react_dars_website/src/App.js
@@ -2,19 +2,151 @@ import React, { Component } from 'react';
 import './App.css'
 import CSGE from './CSGE';
 import TBR from './TBR';
+import saveClasses from './saveClasses';
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state={
-            selectedOption: "GE True",
+            GE1: false,
+            GE2: false,
+            GE3: false,
+            GE4: false,
+            GE5: false,
+            GE6: false,
+            GE7: false,
+            LD1: true,
+            LD2: false,
+            LD3: false,
+            LD4: false,
+            LD5: false,
+            LD6: false,
+            LD7: false,
+            LD8: false,
+            LD9: false,
+            LD10: false,
+            LD11: false,
+            LD12: false,
+            LD13: false,
+            LD14: false,
+            LD15: false,
+            LD16: false,
+            LD17: false,
+            UD1: false,
+            UD2: false,
+            UD3: false,
+            UD4: false,
+            UD5: false,
+            UD6: false,
+            UD7: false,
+            UD8: false,
+            UD9: false,
             TBR1: false,
             TBR2:false,
-            TBR3: false
+            TBR3: false,
+            ST1: false,
+            ST2: false,
+            ST3: false
         };
     }
-    handleOptionChange= changeEvent =>{
-        this.setState({selectedOption: changeEvent.target.value})
+    handGE1 = changeEvent =>{
+        this.setState({GE1: changeEvent.target.checked})
+    }
+    handGE2 = changeEvent =>{
+        this.setState({GE2: changeEvent.target.checked})
+    }
+    handGE3 = changeEvent =>{
+        this.setState({GE3: changeEvent.target.checked})
+    }
+    handGE4 = changeEvent =>{
+        this.setState({GE4: changeEvent.target.checked})
+    }
+    handGE5 = changeEvent =>{
+        this.setState({GE5: changeEvent.target.checked})
+    }
+    handGE6 = changeEvent =>{
+        this.setState({GE6: changeEvent.target.checked})
+    }
+    handGE7 = changeEvent =>{
+        this.setState({GE7: changeEvent.target.checked})
+    }
+    handleLD1= changeEvent =>{
+        this.setState({LD1: changeEvent.target.checked})
+    }
+    handleLD2= changeEvent =>{
+        this.setState({LD2: changeEvent.target.checked})
+    }
+    handleLD3= changeEvent =>{
+        this.setState({LD3: changeEvent.target.checked})
+    }
+    handleLD4= changeEvent =>{
+        this.setState({LD4: changeEvent.target.checked})
+    }
+    handleLD5= changeEvent =>{
+        this.setState({LD5: changeEvent.target.checked})
+    }
+    handleLD6= changeEvent =>{
+        this.setState({LD6: changeEvent.target.checked})
+    }
+    handleLD7= changeEvent =>{
+        this.setState({LD7: changeEvent.target.checked})
+    }
+    handleLD8= changeEvent =>{
+        this.setState({LD8: changeEvent.target.checked})
+    }
+    handleLD9= changeEvent =>{
+        this.setState({LD9: changeEvent.target.checked})
+    }
+    handleLD10= changeEvent =>{
+        this.setState({LD10: changeEvent.target.checked})
+    }
+    handleLD11= changeEvent =>{
+        this.setState({LD11: changeEvent.target.checked})
+    }
+    handleLD12= changeEvent =>{
+        this.setState({LD12: changeEvent.target.checked})
+    }
+    handleLD13= changeEvent =>{
+        this.setState({LD13: changeEvent.target.checked})
+    }
+    handleLD14= changeEvent =>{
+        this.setState({LD14: changeEvent.target.checked})
+    }
+    handleLD15= changeEvent =>{
+        this.setState({LD15: changeEvent.target.checked})
+    }
+    handleLD16= changeEvent =>{
+        this.setState({LD16: changeEvent.target.checked})
+    }
+    handleLD17= changeEvent =>{
+        this.setState({LD17: changeEvent.target.checked})
+    }
+    handleUD1= changeEvent =>{
+        this.setState({UD1: changeEvent.target.checked})
+    }
+    handleUD2= changeEvent =>{
+        this.setState({UD2: changeEvent.target.checked})
+    }
+    handleUD3= changeEvent =>{
+        this.setState({UD3: changeEvent.target.checked})
+    }
+    handleUD4= changeEvent =>{
+        this.setState({UD4: changeEvent.target.checked})
+    }
+    handleUD5= changeEvent =>{
+        this.setState({UD5: changeEvent.target.checked})
+    }
+    handleUD6= changeEvent =>{
+        this.setState({UD6: changeEvent.target.checked})
+    }
+    handleUD7= changeEvent =>{
+        this.setState({UD7: changeEvent.target.checked})
+    }
+    handleUD8= changeEvent =>{
+        this.setState({UD8: changeEvent.target.checked})
+    }
+    handleUD9= changeEvent =>{
+        this.setState({UD9: changeEvent.target.checked})
     }
     handleTBR1= changeEvent =>{
         this.setState({TBR1: changeEvent.target.checked})
@@ -24,8 +156,7 @@ class App extends Component {
     }
     handleTBR3= changeEvent=>{
         this.setState({TBR3: changeEvent.target.checked})
-    }
-
+    } 
     render() {
         return (    
             <form>
@@ -33,27 +164,38 @@ class App extends Component {
                     <h1>Welcome to Class Sync</h1>
                 </div>
                 <div style={{fontSize: "1.25vw"}}>
-                    <b>Have you Completed your General Education?</b>
+                    <b>GEs</b>
                 </div>
-                <div className='GE Completed' style={{marginLeft: "20px"}}>
-                    <label>
-                        <input type="radio" value="GE True" checked={this.state.selectedOption==="GE True"} 
-                        onChange={this.handleOptionChange} />
-                        GE Completed
-                    </label>
-                </div>
-                <div className='GE Not' style={{marginLeft: "20px"}}>
-                    <label>
-                        <input type="radio" value="GE False" checked={this.state.selectedOption==="GE False"}
-                        onChange={this.handleOptionChange} />
-                        GE Not Completed
-                    </label>  
-                </div>
-                <div>
-                    <CSGE
-                    complete={this.state.selectedOption}
-                    />
-                </div>
+                <div style={{marginLeft: "20px"}}>
+                    <div>
+                        <input type="checkbox" id="geReq1" name="Writing I" value="geReq1"/>
+                        Writing I
+                    </div>
+                    <div>
+                        <input type="checkbox" id="geReq2" name="FAH GE 1" value="geReq2"/>
+                        FAH GE (Lit/Cult or Phil/Ling or Visual/Arts)
+                    </div>
+                    <div>
+                        <input type="checkbox" id="geReq3" name="FAH GE 2" value="geReq3"/>
+                        FAH GE (Lit/Cult or Phil/Ling or Visual/Arts)(Different from above)
+                    </div>
+                    <div>
+                        <input type="checkbox" id="geReq4" name="FSC GE (Historical)" value="geReq4"/>
+                        FSC GE (Historical)
+                    </div>
+                    <div>
+                        <input type="checkbox" id="geReq5" name="FSC GE (Social)" value="geReq5"/>
+                        FSC GE (Social)
+                    </div>
+                    <div>
+                        <input type="checkbox" id="geReq6" name="FSC GE (Life Sci)" value="geReq6"/>
+                        FSC GE (Life Sci)
+                    </div>
+                    <div>
+                        <input type="checkbox" id="geReq7" name="geReq7" value="geReq7"/>
+                        Engineering Ethics and Writing(1 from ENGR 181EW to 185EW)
+                    </div>
+                 </div>
                 <div style={{fontSize: "1.25vw"}}>
                     <b>Select Completed Major Requirements:</b>
                 </div>
@@ -61,67 +203,71 @@ class App extends Component {
                     <div><b>Lower Division Courses:</b></div>
                     <div style={{marginLeft: "20px"}}>
                         <div>
-                            <input type="checkbox" id="ldReq1" name="ldReq1" value="ldReq1"/>
+                            <input type="checkbox" id="ldReq1" name="CS1" value="ldReq1" defaultChecked={this.state.LD1}/> 
                             CS 1
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq2" name="ldReq2" value="ldReq2"/>
+                            <input type="checkbox" id="ldReq2" name="CS31" value="ldReq2"/>
                             CS 31
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq3" name="ldReq3" value="ldReq3"/>
+                            <input type="checkbox" id="ldReq3" name="CS32" value="ldReq3"/>
+                            CS 32
+                        </div>
+                        <div>
+                            <input type="checkbox" id="ldReq4" name="CS33" value="ldReq4"/>
                             CS 33
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq4" name="ldReq4" value="ldReq4"/>
+                            <input type="checkbox" id="ldReq5" name="CS 35L" value="ldReq5"/>
                             CS 35L
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq5" name="ldReq5" value="ldReq5"/>
+                            <input type="checkbox" id="ldReq6" name="CS M51A or ECE M16" value="ldReq6"/>
                             CS M51A or ECE M16
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq6" name="ldReq6" value="ldReq6"/>
+                            <input type="checkbox" id="ldReq7" name="Math 31A" value="ldReq6"/>
                             Math 31A
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq7" name="ldReq7" value="ldReq7"/>
+                            <input type="checkbox" id="ldReq8" name="Math 31B" value="ldReq8"/>
                             Math 31B
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq8" name="ldReq8" value="ldReq8"/>
+                            <input type="checkbox" id="ldReq9" name="Math 32A" value="ldReq9"/>
                             Math 32A
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq9" name="ldReq9" value="ldReq9"/>
+                            <input type="checkbox" id="ldReq10" name="Math 32B" value="ldReq10"/>
                             Math 32B
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq10" name="ldReq10" value="ldReq10"/>
+                            <input type="checkbox" id="ldReq11" name="Math33A" value="ldReq11"/>
                             Math 33A
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq11" name="ldReq11" value="ldReq11"/>
+                            <input type="checkbox" id="ldReq12" name="Math 33B" value="ldReq12"/>
                             Math 33B
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq12" name="ldReq12" value="ldReq12"/>
+                            <input type="checkbox" id="ldReq13" name="Math 61" value="ldReq13"/>
                             Math 61
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq13" name="ldReq13" value="ldReq13"/>
+                            <input type="checkbox" id="ldReq14" name="Physics 1A" value="ldReq14"/>
                             Physics 1A
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq14" name="ldReq14" value="ldReq14"/>
+                            <input type="checkbox" id="ldReq15" name="Physics 1B" value="ldReq15"/>
                             Physics 1B
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq15" name="ldReq15" value="ldReq15"/>
+                            <input type="checkbox" id="ldReq16" name="Physics 1C" value="ldReq16"/>
                             Physics 1C
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq16" name="ldReq16" value="ldReq16"/>
+                            <input type="checkbox" id="ldReq17" name="Physics 4AL or 4BL" value="ldReq16"/>
                             Physics 4AL or 4BL
                         </div>
                     </div>
@@ -134,35 +280,35 @@ class App extends Component {
                             CS 111
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq2" name="ldReq2" value="ldReq2"/>
+                            <input type="checkbox" id="udReq2" name="udReq2" value="udReq2"/>
                             CS 118
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq3" name="ldReq3" value="ldReq3"/>
+                            <input type="checkbox" id="udReq3" name="udReq3" value="udReq3"/>
                             CS 131
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq4" name="ldReq4" value="ldReq4"/>
+                            <input type="checkbox" id="udReq4" name="udReq4" value="udReq4"/>
                             CS M151B or ECE M116C
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq5" name="ldReq5" value="ldReq5"/>
+                            <input type="checkbox" id="udReq5" name="udReq5" value="udReq5"/>
                             CS M5151A or ECE M116L                           
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq6" name="ldReq6" value="ldReq6"/>
+                            <input type="checkbox" id="udReq6" name="udReq6" value="udReq6"/>
                             CS 180
                         </div>
                       <div>
-                        <input type="checkbox" id="ldReq7" name="ldReq7" value="ldReq7"/>
+                        <input type="checkbox" id="udReq7" name="udReq7" value="udReq7"/>
                         CS 181
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq8" name="ldReq8" value="ldReq8"/>
+                            <input type="checkbox" id="udReq8" name="udReq8" value="udReq8"/>
                             CS 130 or 152B
                         </div>
                         <div>
-                            <input type="checkbox" id="ldReq9" name="ldReq9" value="ldReq9"/>
+                            <input type="checkbox" id="udReq9" name="udReq9" value="udReq9"/>
                             Statisitics (Stats 100A, Math 170A, Math 170E ECE 131A, CEE 110)
                         </div>
                     </div>
