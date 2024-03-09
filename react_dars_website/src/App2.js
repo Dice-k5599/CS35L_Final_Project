ee80c17253cc59b29cfcd795ba1bd44846983a0b
RyanPosti <ryanposti01@gmail.com>
2024-03-09 13:47:16 -0800

    Commiting change

--- react_dars_website/src/App2.js
+++ react_dars_website/src/App2.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import './App.css'
-import CheckboxList from "./CheckboxList";
+import CheckboxList from "./components/CheckboxList";
 import ClassList from "./ClassList";
 import { ClassData } from "./ClassData";
 
