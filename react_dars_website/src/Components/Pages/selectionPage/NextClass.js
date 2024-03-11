c0ff39beb0380b7e07a8ec9a5f9fea49734ac18b
ahjcgit <ahjcuniv@gmail.com>
2024-03-11 00:32:41 -0700

    Algorithm to find list of recommended classes

---
+++ react_dars_website/src/Components/Pages/selectionPage/NextClass.js
@@ -0,0 +1,29 @@
+import getClassData from "./getClassData.js";
+
+const NextClass = async () => {
+  try {
+    const classesData = await getClassData();
+    const completedClass = classesData.filter(classesData => classesData.completed);
+    const leftClass = classesData.filter(classesData => !classesData.completed);
+
+    // If completed is true it is fed into this list
+    const completedClassCodes = completedClass.map(classesData => classesData.classCode);
+
+    const recommendedClasses = leftClass.filter(leftClass => {
+      // Check if all pre-reqs of the leftClass are met
+      const preReqsMet = leftClass.prereq.every(prereq => completedClassCodes.includes(prereq));
+      return preReqsMet;
+    });
+
+    
+
+    console.log('Recommended Next Classes:', recommendedClasses);
+    return recommendedClasses;
+    return 0;
+  } catch (error) {
+    console.error('Error recommending classes:', error);
+    throw error;
+  }
+};
+
+export default NextClass;
